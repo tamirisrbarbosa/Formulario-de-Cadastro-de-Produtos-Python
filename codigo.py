b036505acd19321fadb66aeaef72fb1791a4e532
tamirisrbarbosa <t4miris.rb@gmail.com>
2025-04-12 21:13:21 -0300

    Concluido

--- codigo.py
+++ codigo.py
@@ -8,7 +8,8 @@ pyautogui.PAUSE = 0.5
     # https://dlp.hashtagtreinamentos.com/python/intensivao/login
 
 # Abrir o navegador padrão (no meu caso, o meu Windows e o brave)  
-pyautogui.press("win")
+pyautogui.press("win")     
+
 pyautogui.write("brave")
 pyautogui.press("enter")
 
@@ -21,7 +22,8 @@ time.sleep(3)
 
 
 # 2 Fazer login
-# Preencher e-mail
+# Preencher
+
 pyautogui.click(x=693, y=513)
 pyautogui.write("irisrbarbosa.contato@gmail.com")
 
@@ -33,9 +35,53 @@ pyautogui.write("senhadementirinha")
 pyautogui.press("tab")
 pyautogui.press("enter")
 
+# Espera de 3 segundos
+time.sleep(3)
 
 # 3 Importar a base de produtos pra cadastrar
+import pandas
+
+tabela = pandas.read_csv("produtos.csv")
+print(tabela)
+
 
 # 4 Cadastrar um produto
 
-    # 5 Repetir o processo de cadastro até o fim
+for linha in tabela.index: # cadastrando cada linha da tabela
+    pyautogui.click(x=737, y=361)
+
+    codigo = tabela.loc[linha, "codigo"]
+    pyautogui.write(codigo)
+
+    pyautogui.press("tab") # passar para próximo campo
+    marca = tabela.loc[linha, "marca"]
+    pyautogui.write(marca)
+
+    pyautogui.press("tab") # passar para próximo campo
+    tipo = tabela.loc[linha, "tipo"]
+    pyautogui.write(tipo)
+
+    pyautogui.press("tab") # passar para próximo campo
+    categoria = str(tabela.loc[linha, "categoria"])  # str() para transformar em String = texto
+    pyautogui.write(categoria)
+
+    pyautogui.press("tab") # passar para próximo campo
+    preco_unitario =str(tabela.loc[linha, "preco_unitario"])
+    pyautogui.write(preco_unitario)
+
+    pyautogui.press("tab") # passar para próximo campo
+    custo = str(tabela.loc[linha, "custo"])
+    pyautogui.write(custo)
+
+    pyautogui.press("tab") # passar para próximo campo
+    obs = str(tabela.loc[linha, "obs"])
+
+    if obs != "nan":          
+        pyautogui.write(obs)
+
+    pyautogui.press("tab") # passando para o botão enviar
+    pyautogui.press("enter")
+
+    pyautogui.scroll(100000) # subir para o início da página
+
+# 5 Repetir o processo de cadastro até o fim
